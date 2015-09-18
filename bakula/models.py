e6ae260dbee369b54d86ee09302bbf48c9c41ac4
Eric Perry <eric@ericjperry.com>
2015-09-18 12:55:04 -0400

    Added account creation line back in

--- bakula/models.py
+++ bakula/models.py
@@ -65,3 +65,4 @@ def initialize_models(config):
 
     # The first user in the DB will be the admin user. Ignore errors.
     from bakula.security import iam
+    iam.create('admin', config.get('admin_password', 'secret'))
