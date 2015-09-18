c4671d7ceaf5574dd9947fa22965e846dcc9adc1
Kris Siegel <krissiegel@gmail.com>
2015-09-18 04:32:34 -0400

    Minor updates; unit tests don't work so incomplete

--- bakula/services/event.py
+++ bakula/services/event.py
@@ -9,6 +9,7 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+
 from bottle import Bottle, HTTPResponse, request, response, FileUpload
 
 from bakula.bottle import configuration
---
+++ bakula/services/event_test.py
@@ -0,0 +1,25 @@
+# Copyright 2015 Immuta, Inc. Licensed under the Immuta Software License
+# Version 0.1 (the "License"); you may not use this file except in
+# compliance with the License. You may obtain a copy of the License at
+#
+#    http://www.immuta.com/licenses/Immuta_Software_License_0.1.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import unittest
+from bakula.services import event
+from webtest import TestApp
+
+test_app = TestApp(event.app)
+
+class EventTest(unittest.TestCase):
+
+    def test_post_event(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
