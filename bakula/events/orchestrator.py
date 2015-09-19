05e49caa673f4c293ff9cd77cb18ea05be4a7dc4
Kris Siegel <krissiegel@gmail.com>
2015-09-18 22:22:57 -0400

    Added query to get registered info for containers Added comments

--- bakula/events/orchestrator.py
+++ bakula/events/orchestrator.py
@@ -16,6 +16,7 @@
 #   under the License.
 from bakula.events.inboxer import Inboxer
 from bakula.docker.dockeragent import DockerAgent
+from bakula.models import Registration, resolve_query
 import uuid
 
 # This class handles the event handling of the inboxer and, when a threshold is hit,
@@ -26,12 +27,21 @@ class Orchestrator:
         self.inboxer = inboxer
         self.inboxer.on("received", self.__handle_inbox_received_event)
 
+    # Get listing of registered containers filtered by topic
+    def __get_registered_containers(self, topic):
+        query = Registration.select().where(Registration.topic == topic)
+        return resolve_query(query)
+
     # This event handler is executed every time a file is put into the master inbox
     # The data argument includes a property specify the topic that was appended to
     def __handle_inbox_received_event(self, data):
+        # Get a list of all files in the inbox delineated by the topic in the event
         inbox_list = self.inboxer.get_inbox_list(data["topic"])
 
+        # Are there enough items in the inbox to trigger the threshold?
         if len(inbox_list) > 1: # This is just a temporary threshold
+            # Promote all files in the inbox, delineated by topic, to a directory for
+            # a container to mount; returns a list of created inboxes
             container_inboxes = self.inboxer.promote_to_container_inbox(data["topic"], str(uuid.uuid4()))
 
             for container_inbox in container_inboxes:
