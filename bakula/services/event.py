aadb8f64df5b5adc8e2a33e5b9732c4d976113cd
Eric Perry <eric@ericjperry.com>
2015-09-28 10:34:27 -0400

    Fixed a couple license headers on files and updated README

--- bakula/services/event.py
+++ bakula/services/event.py
@@ -1,15 +1,19 @@
-# Copyright 2015 Immuta, Inc. Licensed under the Immuta Software License
-# Version 0.1 (the "License"); you may not use this file except in
-# compliance with the License. You may obtain a copy of the License at
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
 #
-#    http://www.immuta.com/licenses/Immuta_Software_License_0.1.txt
+#   http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
-
+#   Unless required by applicable law or agreed to in writing,
+#   software distributed under the License is distributed on an
+#   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#   KIND, either express or implied.  See the License for the
+#   specific language governing permissions and limitations
+#   under the License.
 from bottle import Bottle, HTTPResponse, request, response, FileUpload
 
 from bakula.bottle import configuration
